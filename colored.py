0d86032a4c8409311a999075fc55dfb6684a3e29
mhuda2019 <48696696+mhuda2019@users.noreply.github.com>
2023-04-04 22:10:51 +0330

    Delete colored.py

--- colored.py
+++
@@ -1,8 +0,0 @@
-import turtle
-v=turtle.Turtle()
-colors=['red','purple','blue','green','orange','yellow']
-for x in range(100):
-    v.pencolor(colors[x%6])
-    v.forward(x)
-    v.left(59)
-turtle.done()
